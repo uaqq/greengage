tvesely/yugabyte-db

Add declaration-level assertions for compile-time checks
Those new assertions can be used at file scope, outside of any function
for compilation checks.  This commit provides implementations for C and
C++, and fallback implementations.

Author: Peter Smith
Reviewed-by: Andres Freund, Kyotaro Horiguchi, Dagfinn Ilmari Mannsåker,
Michael Paquier
Discussion: https://postgr.es/m/201DD0641B056142AC8C6645EC1B5F62014B8E8030@SYD1217

src/postgres/src/backend/utils/adt/lockfuncs.c
@@ -36,12 +36,18 @@ const char *const LockTagTypeNames[] = {
 	"advisory"
 };
 
+StaticAssertDecl(lengthof(LockTagTypeNames) == (LOCKTAG_ADVISORY + 1),
+				 "array length mismatch");
+
 /* This must match enum PredicateLockTargetType (predicate_internals.h) */
 static const char *const PredicateLockTagTypeNames[] = {
 	"relation",
 	"page",
 	"tuple"
 };
+
+StaticAssertDecl(lengthof(PredicateLockTagTypeNames) == (PREDLOCKTAG_TUPLE + 1),
+				 "array length mismatch");
 
 /* Working status for pg_lock_status */
 typedef struct

uaqq/greengage

Changes in 7X:
1. Fix assert for config_group_names, use ___CONFIG_GROUP_COUNT as last element.
2. Fix assert for LockTagTypeNames, use LOCKTAG_LAST_TYPE as last element.
3. Remove assert for dbObjectTypePriority. It will be fixed in the next commit.
(cherry picked from commit f1f10a1ba9e17e606a7b217ccccdd3cc4d8cb771)

src/backend/utils/adt/lockfuncs.c
@@ -44,12 +44,18 @@ const char *const LockTagTypeNames[] = {
 	"frozenid"
 };
 
+StaticAssertDecl(lengthof(LockTagTypeNames) == (LOCKTAG_LAST_TYPE + 1),
+				 "array length mismatch");
+
 /* This must match enum PredicateLockTargetType (predicate_internals.h) */
 static const char *const PredicateLockTagTypeNames[] = {
 	"relation",
 	"page",
 	"tuple"
 };
+
+StaticAssertDecl(lengthof(PredicateLockTagTypeNames) == (PREDLOCKTAG_LAST + 1),
+				 "array length mismatch");
 
 /* Working status for pg_lock_status */
 typedef struct